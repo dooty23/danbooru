acdbebe6c0dfcd5e601efbd5a733ba267da77cdd
dooty23 <43501583+dooty23@users.noreply.github.com>
2018-09-22 12:33:21 -0500

    Add files via upload

---
+++ application.rb
@@ -0,0 +1,54 @@
+require_relative 'boot'
+require 'rails/all'
+
+Bundler.require(*Rails.groups)
+
+require_relative "danbooru_default_config"
+require_relative "danbooru_local_config"
+
+module Danbooru
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.1
+    config.active_record.schema_format = :sql
+    config.encoding = "utf-8"
+    config.filter_parameters += [:password]
+    config.assets.enabled = true
+    config.assets.version = '1.0'
+    config.autoload_paths += %W(#{config.root}/app/presenters #{config.root}/app/logical #{config.root}/app/mailers)
+    config.plugins = [:all]
+    config.time_zone = 'Eastern Time (US & Canada)'
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {:enable_starttls_auto => false}
+    config.action_mailer.perform_deliveries = true
+    config.log_tags = [->(req) {"PID:#{Process.pid}"}]
+    config.action_controller.action_on_unpermitted_parameters = :raise
+    config.force_ssl = true
+
+    if Rails.env.production? && Danbooru.config.ssl_options.present?
+      config.ssl_options = Danbooru.config.ssl_options
+    else
+      config.ssl_options = {
+        hsts: false,
+        secure_cookies: false,
+        redirect: { exclude: ->(request) { true } }
+      }
+    end
+
+    if File.exists?("#{config.root}/REVISION")
+      config.x.git_hash = File.read("#{config.root}/REVISION").strip
+    elsif system("type git > /dev/null && git rev-parse --show-toplevel > /dev/null")
+      config.x.git_hash = %x(git rev-parse --short HEAD).strip
+    else
+      config.x.git_hash = nil
+    end
+
+    config.after_initialize do
+      Rails.application.routes.default_url_options = {
+        host: Danbooru.config.hostname,
+      }
+    end
+  end
+
+  I18n.enforce_available_locales = false
+end
---
+++ deploy.rb
@@ -0,0 +1,9 @@
+set :stages, %w(production development staging)
+set :default_stage, "staging"
+set :application, "danbooru"
+set :repo_url,  "git://https://github.com/dooty23/danbooru"
+set :scm, :git
+set :deploy_to, "/var/www/danbooru2"
+set :rbenv_ruby, "2.5.0"
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle')
+set :branch, ENV.fetch("branch", "master")
